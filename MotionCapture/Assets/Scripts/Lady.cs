f99a33c7839961b90a326b66f4a0873f4085958d
zenja90907 <55218478+zenja90907@users.noreply.github.com>
2019-10-31 10:43:02 +0800

    角色控制類別方法

---
+++ MotionCapture/Assets/Scripts/Lady.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Lady : MonoBehaviour
+{
+    //定義方法
+    //修飾詞 傳回類型 方法名稱(參數){敘述}
+    //void無回傳
+    ///<summary>
+    ///角色的走路功能
+    ///</summary>
+    private void Walk()
+    {
+
+    }
+
+    ///<summary>
+    ///攻擊
+    ///</summary>
+    private void Attack()
+    {
+
+    }
+
+    ///<summary>
+    ///跳躍
+    ///</summary>
+    private void Jump()
+    {
+
+    }
+
+    ///<summary>
+    ///受傷
+    ///</summary>
+    private void Hurt()
+    {
+
+    }
+
+    ///<summary>
+    ///死亡
+    ///</summary>
+    private void Dead()
+    {
+
+    }
+    
+}
